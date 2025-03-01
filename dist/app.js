79369bc6e87e2d3a9b6b8635d3fe2afe6bf2aff9
Ahsan Tonmoy <ahosant82@gmail.com>
2025-03-02 01:27:10 +0600

    deploy

--- dist/app.js
+++ dist/app.js
@@ -5,12 +5,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
-const product_routes_1 = require("./app/modules/product/product.routes");
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use((0, cors_1.default)());
 //
-app.use('/api', product_routes_1.productsRoutes);
+// app.use('/api', productsRoutes)
 // app.use('/api', orderRoutes)
 //
 app.get('/', (req, res) => {
